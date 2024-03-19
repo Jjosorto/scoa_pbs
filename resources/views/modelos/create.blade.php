a9b52e5bfe411f7ca4d7b895398a95de769d8930
jjosorto <jhonatanosorto39@gmail.com>
2024-03-18 21:23:23 -0600

    añadiendo modulos modelo, migracion de nuevo atributo imagen url

---
+++ resources/views/modelos/create.blade.php
@@ -0,0 +1,79 @@
+<x-app-layout>
+    <div class="row justify-content-center ">
+        <div class="col-md-6 bg-white my-4 py-4 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="container mt-5">
+                <form id="crearModelos" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="nombreModelo">Nombre de Modelo:</label>
+                        <input type="text" class="form-control" id="nombreModelo" name="nombreModelo" required>
+                    </div>
+                    <div class="form-group my-4">
+                        <label for="id_marca">Marcas:</label>
+                        <select class="form-control" name="id_marca" id="id_marca">
+                            @foreach ($marcas as $marca)
+                            <option value="{{ $marca->id }}">{{ $marca->nombreMarca }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group my-4">
+                        <label for="id_categoria">Categorias:</label>
+                        <select class="form-control" name="id_categoria" id="id_categoria">
+                            @foreach ($categorias as $categoria)
+                            <option value="{{ $categoria->id }}">{{ $categoria->nombreCategoria }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group mt-3">
+                        <button type="submit" class="btn btn-dark">Crear</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+ <!-----------Script/---------------->
+    <script>
+        $(document).ready(function() {
+            $('#crearModelos').submit(function(e) {
+                e.preventDefault();
+
+                // Obtén los datos del formulario
+                var formData = $(this).serialize();
+
+                // Realiza la solicitud AJAX
+                $.ajax({
+                    type: 'POST',
+                    url: "{{ route('modelos.store') }}", // validar de que la ruta sea correcta
+                    data: formData,
+                    success: function(response) {
+                        Swal.fire(
+                            'Acción Exitosa',
+                            'La acción se realizó con éxito.',
+                            'success'
+                        ).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = "{{ route('modelos.index') }}";
+                            }
+                        });
+
+                    },
+                    error: function(xhr) {
+                        // Maneja los errores de validación u otros errores
+                        var errors = xhr.responseJSON;
+                        Swal.fire(
+                            'Error',
+                            'No se pudo realizar la acción.',
+                            'error'
+                        ).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.reload();
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    </script>
+
+
+</x-app-layout>
---
+++ resources/views/modelos/index.blade.php
@@ -0,0 +1,112 @@
+<x-app-layout>
+    <div class="container my-5 bg-dark shadow p-3 mb-5 bg-body-tertiary rounded" style="border-radius: 15px; height: 100%">
+
+        <div class="container py-5 px-5 ">
+            <div class="row">
+                <div class="col-12 mb-5" style="text-align: end">
+                    <a href="{{ route('modelos.create') }}">
+                        <button class="btn btn-dark"><i class="fa-regular fa-square-plus" style="color: #ffffff;"></i>
+                            Nuevo Modelo
+                        </button>
+                    </a>
+                </div>  
+            </div>
+            <table id="table" class="display">
+                <thead>
+                    <tr>
+                        <th>Nombre de Modelo</th>
+                        <th>Marca</th>
+                        <th>Categoria</th>
+                        <th>Estado</th>
+                        <th style="text-align: end">Opciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($modelos as $modelo)
+                    <tr>
+                        <td>{{ $modelo->nombreModelo }}</td>
+                        <td>{{ $modelo->marca->nombreMarca }}</td>
+                        <td>{{ $modelo->categoria->nombreCategoria }}</td>
+                        <td>@if($modelo->estadoModelo == 0)
+                            Inactivo
+                            @else
+                            Activo
+                            @endif
+                        </td>
+                        <td style="text-align: end">
+                            <a href="{{ route('modelos.edit', $modelo->id) }}"><button class="btn btn-dark"><i class="fa-solid fa-pen-to-square" style="color: #ffffff;"></i></button></a>
+                            <button class="btn btn-secondary btn-desactivar" data-id="{{ $modelo->id }}"><i class="fas fa-ban" style="color: #ffffff;"></i></button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+ <!-----------Script/---------------->
+    <script>
+        $(document).ready(function() {
+
+            let table = $('#table').DataTable();
+
+            $('#table').on('click', '.btn-desactivar', function() {
+                var id = $(this).data('id');
+                console.log('entró')
+                Swal.fire({
+                    title: "Desactivar Modelo",
+                    text: "¿Desea continuar?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: "Confirmar",
+                    cancelButtonText: "Cancelar",
+                    customClass: {
+                        confirmButton: 'btn btn-dark mx-2',
+                        cancelButton: 'btn btn-secondary mx-2'
+                    },
+                    showClass: {
+                        popup: 'swal2-noanimation',
+                        backdrop: 'swal2-noanimation'
+                    },
+                    buttonsStyling: false
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        var url = "{{ route('modelos.desactive', ':id') }}";
+                        url = url.replace(':id', id);
+
+                        $.ajax({
+                            type: 'get',
+                            url: url,
+                            success: function(response) {
+                                Swal.fire(
+                                    'Acción Exitosa',
+                                    'La acción se realizó con éxito.',
+                                    'success'
+                                ).then((result) => {
+                                    if (result.isConfirmed) {
+                                        window.location.href =
+                                            "{{ route('modelos.index') }}";
+                                    }
+                                });
+
+                            },
+                            error: function(xhr) {
+                                // Maneja los errores de validación u otros errores
+                                var errors = xhr.responseJSON;
+                                Swal.fire(
+                                    'Error',
+                                    'No se pudo realizar la acción.',
+                                    'error'
+                                ).then((result) => {
+                                    if (result.isConfirmed) {
+                                        window.location.href =
+                                            "{{ route('modelos.index') }}";
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    </script>
+</x-app-layout>
